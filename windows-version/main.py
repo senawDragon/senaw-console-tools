50f6f2ef8fe3ef16821dd76f91491af904f617f3
senawDragon <98814168+senawDragon@users.noreply.github.com>
2022-04-01 09:35:27 +0200

    Update main.py

--- windows-version/main.py
+++ windows-version/main.py
@@ -9,6 +9,7 @@ import webbrowser
 import tempfile
 import platform
 import os
+from datetime import datetime
 import socket
 import re
 import uuid
@@ -89,6 +90,7 @@ while True:
               "IP_MEME | This command allows you to run a D4DJ IP meme. "
               "This will display your actual public IP so be careful\n"
               "clear | This command allows you to clear out all previous commands\n"
+              "portscan | This command allows you to scan ports, please consider consequences and your nations regulations and laws if you are planning to scan networks beside your own\n"
               "open | This command allows you to open software\n"
               "web_browse | This command allows you to visit a website\n"
               "web_search | This command allows you to search using google\n"
@@ -138,7 +140,44 @@ while True:
         label = Label(root, text=str(ip), font="-weight bold -size 55").pack()
 
         root.mainloop()
+        
+    if stdin == "portscan":
+        if len(sys.argv) == 2:
+     
+    
+            target = socket.gethostbyname(sys.argv[1])
+        else:
+            print("Invalid amount of Argument")
 
+
+            print("-" * 50)
+            print("Scanning Target: " + target)
+            print("Scanning started at:" + str(datetime.now()))
+            print("-" * 50)
+
+        try:
+
+
+            for port in range(1,65535):
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket.setdefaulttimeout(1)
+
+
+                result = s.connect_ex((target,port))
+                if result ==0:
+                    print("Port {} is open".format(port))
+                s.close()
+
+        except KeyboardInterrupt:
+                print("\n Exiting Program !!!!")
+                sys.exit()
+        except socket.gaierror:
+                print("\n Hostname Could Not Be Resolved !!!!")
+                sys.exit()
+        except socket.error:
+                print("\ Server not responding !!!!")
+                sys.exit()
+    
     if stdin == "downloads":
         print("audacity\n"
               "blender\n"
